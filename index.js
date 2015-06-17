858e97cddc4c2039dd75384420fa82d45a1ce125
Kamil Figiela <kamil.figiela@gmail.com>
2015-06-17 18:56:09 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var glob = require('glob');
+var spawn = require('child_process').spawn;
+var fs = require('fs')
+
+
+function GhcCompiler(config) {
+  if (config == null) config = {};
+  var options = config.plugins && config.plugins.ghcjs;
+  this.options = options
+  console.log(options)
+}
+
+GhcCompiler.prototype.brunchPlugin = true;
+GhcCompiler.prototype.type = 'javascript';
+GhcCompiler.prototype.extension = 'ghcjs';
+
+GhcCompiler.prototype.compile = function(data, path, callback) {
+  var _this = this;
+  if(path == this.options.placeholder) {
+    console.log("Running cabal")
+    cabal = spawn('cabal', ['install', '--ghcjs'], {},{ stdio: 'inherit' });
+
+    cabal.on('close', function (code) {
+      console.log('Cabal exited with code ' + code);
+      if(code == 0) {
+        fs.readFile('dist/build/'+_this.options.projectName+'/'+_this.options.projectName+'.jsexe/all.js', 'utf-8', function(err, data) {
+          callback(null, {data: data});
+        });
+      } else {
+        callback("Cabal failed", null);
+      }
+    });
+  } else {
+    callback(null, "");
+    
+  }};
+
+GhcCompiler.prototype.getDependencies = function(data, path, callback) {
+  if(path == this.options.placeholder) {
+    glob("**/*.hs", {}, function (er, files) {
+      callback(null, files)
+    });
+  } else {
+    callback(null, []);
+  }
+};
+
+
+module.exports = GhcCompiler;
