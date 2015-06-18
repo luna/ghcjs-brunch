42cc8f2409790945671339c90d54a0ee5babdb29
Kamil Figiela <kamil.figiela@gmail.com>
2015-06-18 17:00:44 +0200

    Rebuild only if necessary

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var glob = require('glob');
 var spawn = require('child_process').spawn;
 var fs = require('fs')
-
+var exec = require('shelljs').exec;
 
 function GhcCompiler(config) {
   if (config == null) config = {};
   var options = config.plugins && config.plugins.ghcjs;
   this.options = options
+  this.outfile = 'dist/build/'+this.options.projectName+'/'+this.options.projectName+'.jsexe/all.js'
+  this.globPattern = "app/**/*.hs"
 }
 
 GhcCompiler.prototype.brunchPlugin = true;
@@ -14,38 +16,65 @@ GhcCompiler.prototype.type = 'javascript';
 GhcCompiler.prototype.extension = 'ghcjs';
 
 GhcCompiler.prototype.compile = function(data, path, callback) {
-  var _this = this;
+  var _this = this
   if(path == this.options.placeholder) {
-    console.log("Running cabal")
-    cabal = spawn('cabal', ['install', '--ghcjs'], {},{ stdio: 'inherit' });
-
-    cabal.on('close', function (code) {
-      console.log('Cabal exited with code ' + code);
-      if(code == 0) {
-        fs.readFile('dist/build/'+_this.options.projectName+'/'+_this.options.projectName+'.jsexe/all.js', 'utf-8', function(err, compiled) {
-          
-          var allsource = "(function(){\n " + data + "; \n\n" + compiled + "\n})();"
-          
-          callback(null, {data: allsource});
-        });
+    this.recompileIfChanged(function(shouldRebuild) {
+      if(shouldRebuild) {
+        _this.rebuild(data, callback)
       } else {
-        callback("Cabal failed", null);
+        _this.assembly(data, callback);
+        console.log("Cabal sources not changed, skipping")
       }
     });
   } else {
     callback(null, "");
-    
-  }};
+  }
+};
+
+GhcCompiler.prototype.assembly = function(data, callback) {
+  fs.readFile(this.outfile, 'utf-8', function(err, compiled) {
+    var allsource = "(function(){\n " + data + "; \n\n" + compiled + "\n})();"
+    callback(null, {data: allsource});
+  });  
+}
+
+GhcCompiler.prototype.rebuild = function(data, callback) {
+  var _this = this
+  console.log("Running cabal install...")
+  exec('cabal install --ghcjs', function(code, output){
+    if(code == 0) {
+      console.log("Cabal finished successfully")
+      _this.assembly(data, callback)
+    } else {
+      callback("Cabal failed (code: " + code + ")", null);
+    }
+  });
+}
+
+GhcCompiler.prototype.sourceFiles = function(callback) {
+  var _this = this
+  glob(this.globPattern, {}, function (err, files) {
+    if(files) files.push(_this.options.projectName + ".cabal")
+    callback(err, files)
+  });
+}
+GhcCompiler.prototype.recompileIfChanged = function(callback) {
+  var _this = this;
+  this.sourceFiles(function (err, files) { // TODO: add error handling
+    var max_mtime = Math.max.apply(null, files.map(function(name){ return (fs.statSync(name) || {mtime: ""}).mtime}).sort())
+    var last_compiled = (fs.statSync(_this.outfile) || {mtime: ""}).mtime
+    callback(process.env.RECOMPILE || max_mtime > last_compiled)
+  });
+}
 
 GhcCompiler.prototype.getDependencies = function(data, path, callback) {
   if(path == this.options.placeholder) {
-    glob("**/*.hs", {}, function (er, files) {
+    this.sourceFiles(function (er, files) {
       callback(null, files)
     });
   } else {
     callback(null, []);
   }
 };
-
 
 module.exports = GhcCompiler;
