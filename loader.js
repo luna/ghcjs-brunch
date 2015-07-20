dbd19dfa7ee1d8cdd6e56627e232682666d8f229
Kamil Figiela <kamil.figiela@gmail.com>
2015-07-21 00:25:37 +0200

    Fix z-index of log

--- loader.js
+++ loader.js
@@ -1,4 +1,4 @@
-$("body").append('<div style="position:absolute; width:100%; height: 100%; overflow: auto;"><pre id="log" style="white-space: pre-wrap; word-wrap: break-word; padding: 10px; z-index: -1;">Connecting…\n</pre></div>');
+$("body").append('<div style="position:absolute; width:100%; height: 100%; overflow: auto; z-index: -1;"><pre id="log" style="white-space: pre-wrap; word-wrap: break-word; padding: 10px; ">Connecting…\n</pre></div>');
 var rs = io("http://localhost:9886");
 rs.emit("reload");
 rs.on('stdout', function(msg) {
